0c476e2656c09baefed9ec10812f49b708857b22
cqol_77 <cqol_77@126.com>
2014-01-14 10:44:51 +0800

    updatat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function (grunt) {
+    var timestamp = grunt.template.today("mmddHHMM");
     grunt.initConfig({
         pkg:grunt.file.readJSON('package.json'),
         lib:{
@@ -11,9 +12,15 @@ module.exports = function (grunt) {
                 js:'build/tts_union_media.js',
                 css:'build/p4p-2.0.css',
                 img:'build/',
-                center:'build/tts_union_center.js'
+                center:'build/tts_union_center.js',
+                bubble:'build/tts_union_bubble.js',
+                slides:'build/tts_union_slides.js'
             },
-            debug:'build/debug/tts_union_media.js'
+            //debug:'build/debug/tts_union_media.js'
+            debug:{
+                media:'build/debug/tts_union_media.js',
+                slides:'build/debug/tts_union_slides.js'
+            }
         },
         union:{
             file:{
@@ -25,6 +32,16 @@ module.exports = function (grunt) {
         center:{
             file:{
                 js:'js/tts_union_center.js'
+            }
+        },
+        bubble:{
+            file:{
+                js:'js/tts_union_bubble.js'
+            }
+        },
+        slides:{
+            file:{
+                js:'js/tts_union_slides.js'
             }
         },
         concat:{
@@ -39,13 +56,28 @@ module.exports = function (grunt) {
             union:{
                 options:{
                     banner:'/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+                        '<%= grunt.template.today("yyyy-mm-dd_HH_MM_ss") %> */'
                 },
                 src:['<%= lib.ttsui %>', '<%= union.file.js %>'],
-                dest:'<%= build.debug %>'
+                dest:'<%= build.debug.media %>'
+            },
+            tmt:{
+                options:{
+                    banner:'/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd_HH_MM_ss") %> */'
+                },
+                src:['<%= lib.ttsui %>', 'js/insetTmt.js'],
+                dest:'build/debug/insetTmt.js'
             }
+            /*slides:{
+                src:['<%= lib.ttsui %>', '<%= slides.file.js %>'],
+                dest:'<%= build.debug.slides %>'
+            }*/
         },
         uglify:{
+            options:{
+                banner:'/*! <%= pkg.name %> - by cqol_77 <%= grunt.template.today("yyyy-mm-dd_HH_MM_ss") %> */\n'
+            },
             lib:{
                 options:{
                     banner:'/*!  <%= pkg.name %> -  by cqol_77 <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -56,22 +88,43 @@ module.exports = function (grunt) {
             },
             union:{
                 options:{
-                    banner:'/*! <%= pkg.name %> - by cqol_77 <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                    banner:'/*! <%= pkg.name %> - by cqol_77 <%= grunt.template.today("yyyy-mm-dd_HH_MM_ss") %> */\n'
                 },
                 files:{
-                    '<%= build.file.js %>':'<%= build.debug %>'
+                    '<%= build.file.js %>':'<%= build.debug.media %>'
                 }
             },
             center:{
                 options:{
+                    banner:'/*! <%= pkg.name %> - by cqol_77 <%= grunt.template.today("yyyy-mm-dd_HH_MM_ss") %> */\n'
+                },
+                files:{
+                    'dist/tts_union_center.js':'<%= center.file.js %>'
+                }
+            },
+            bubble:{
+                options:{
                     banner:'/*! <%= pkg.name %> - by cqol_77 <%= grunt.template.today("yyyy-mm-dd") %> */\n'
                 },
                 files:{
-                    '<%= build.file.center %>':'<%= center.file.js %>'
+                    '<%= build.file.bubble %>':'<%= bubble.file.js %>'
+                }
+            },
+            slides:{
+                files:{
+                    '<%= build.file.slides %>':'<%= slides.file.js %>'
+                }
+            },
+            tmt:{
+                files: {
+                    'build/insetTmt.js': 'build/debug/insetTmt.js'
                 }
             }
         },
         jshint:{
+            options:{
+                jshintrc:'.jshintrc'
+            },
             union:{
                 options:{
                     jshintrc:'.jshintrc'
@@ -87,22 +140,51 @@ module.exports = function (grunt) {
                 files:{
                     src:['<%= center.file.js %>']
                 }
+            },
+            bubble:{
+                options:{
+                    jshintrc:'.jshintrc'
+                },
+                files:{
+                    src:['<%= bubble.file.js %>']
+                }
+            },
+            slides:{
+                options:{
+                    jshintrc:'.jshintrc'
+                },
+                files:{
+                    src:['<%= slides.file.js %>']
+                }
+            },
+            tmt:{
+                options:{
+                    jshintrc:'.jshintrc'
+                },
+                files:{
+                    src:['js/insetTmt.js']
+                }
             }
         },
         replace:{
             options:{
                 variables:{
-                    'timestamp':'<%= new Date().getTime() %>'
+                    'timestamp':timestamp
                 }
             },
             union:{
                 files:[
-                    {src:['<%= build.debug %>'], dest:'<%= build.debug %>'}
+                    {src:['<%= build.debug.media %>'], dest:'<%= build.debug.media %>'}
                 ]
             },
             center:{
                 files:[
-                    {src:['<%= center.file.js %>'], dest:'<%= center.file.js %>'}
+                    {src:['dist/tts_union_center.js'], dest:'dist/tts_union_center.js'}
+                ]
+            },
+            tmt:{
+                files:[
+                    {src:['build/debug/insetTmt.js'], dest:'build/debug/insetTmt.js'}
                 ]
             }
         },
@@ -120,8 +202,29 @@ module.exports = function (grunt) {
             },
             combine:{
                 files:{
-                    '<%= build.file.css %>':['<%= union.file.css %>']
+                    '<%= build.file.css %>':['<%= union.file.css %>'],
+                    'build/insetTmt.css':['css/insetTmt.css'],
+                    'build/p4p-2.0.css':['css/p4p-2.0.css']
                 }
+            }
+        },
+        compress: {
+            tmt: {
+                options: {
+                    archive: 'dist/img.taotaosou.cn-browser-static-tmt<%= grunt.template.today("yyyy-mm-dd_HH_MM_ss") %>.tar',
+                    mode: 'tar'
+                },
+                files: [
+                    {expand: true, cwd: 'build/', src: ['**'], dest: 'tmt/'}
+                ]
+            }
+        },
+        clean: {
+            base: {
+                src: ['copy', 'build', 'dist/tmt', 'dist/qzone', 'dist/taotaolink']
+            },
+            compress: {
+                src: ['dist/*.tar']
             }
         }
     });
@@ -134,6 +237,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
@@ -142,6 +246,29 @@ module.exports = function (grunt) {
         grunt.task.run('concat:union');
         grunt.task.run('replace:union');
         grunt.task.run('uglify:union');
+    });
+    grunt.registerTask('tmt', 'tts tmt', function () {
+        grunt.task.run('clean');
+        grunt.task.run('cssmin');
+        //grunt.task.run('jshint:tmt');
+        grunt.task.run('concat:tmt');
+        grunt.task.run('replace:tmt');
+        grunt.task.run('uglify:tmt');
+
+        grunt.task.run('jshint:union');
+        grunt.task.run('concat:union');
+        grunt.task.run('replace:union');
+        grunt.task.run('uglify:union');
+
+        grunt.task.run('jshint:bubble');
+        grunt.task.run('uglify:bubble');
+        grunt.task.run('compress:tmt');
+
+        grunt.task.run('jshint:center');
+        grunt.task.run('uglify:center');
+        grunt.task.run('replace:center');
+
+        grunt.task.run('clean:base');
     });
     grunt.registerTask('lib', 'tts union', function () {
         //grunt.task.run('jshint:union');
@@ -152,6 +279,15 @@ module.exports = function (grunt) {
         //grunt.task.run('jshint:union');
         grunt.task.run('jshint:center');
         grunt.task.run('uglify:center');
+        grunt.task.run('replace:center');
+    });
+    grunt.registerTask('bubble', 'tts union', function () {
+        grunt.task.run('jshint:bubble');
+        grunt.task.run('uglify:bubble');
+    });
+    grunt.registerTask('slides', 'tts union', function () {
+        grunt.task.run('jshint:slides');
+        grunt.task.run('uglify:slides');
     });
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
